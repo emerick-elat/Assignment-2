ba6965bf8aa3888ee065ec6e2e8ee3f4bb142133
Aymerick.Fikeuelat <Aymerick.Fikeuelat@NBA-126-V1-LT>
2024-09-11 12:20:53 +0300

    Add project files.

---
+++ Utilities/Config.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Utilities
+{
+    public class Config
+    {
+        public static async Task<IPEndPoint> GetEndPoint()
+        {
+            var hostName = Dns.GetHostName();
+            IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+            // This is the IP address of the local machine
+            IPAddress localIpAddress = localhost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(localIpAddress, 11_000);
+            return ipEndPoint;
+        }
+
+    }
+}
